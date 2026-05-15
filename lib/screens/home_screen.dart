c4676ae769d52b72b21dadfd84a514b252dcdab8
effileo <fitaalemayehu12@gmail.com>
2026-05-15 09:50:00 +0300

    Build HomeScreen with bottom navigation

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import '../utils/constants.dart';
+import './search_screen.dart';
+import './catalog_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = const [
+    SearchScreen(),
+    CatalogScreen(),
+  ];
+
+  final List<String> _titles = const [
+    'Search Books',
+    'My Catalog',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _titles[_currentIndex],
+          style: AppTextStyles.headingLarge.copyWith(color: Colors.white),
+        ),
+        backgroundColor: AppColors.primary,
+        actions: [
+          if (_currentIndex == 0)
+            IconButton(
+              icon: const Icon(Icons.info_outline),
+              onPressed: () {
+                showAboutDialog(
+                  context: context,
+                  applicationName: AppConstants.appName,
+                  applicationVersion: '1.0.0',
+                  children: [
+                    const Text('A digital library catalog powered by Open Library API.'),
+                  ],
+                );
+              },
+            ),
+        ],
+      ),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        selectedItemColor: AppColors.primary,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_books),
+            label: 'My Catalog',
+          ),
+        ],
+      ),
+      floatingActionButton: _currentIndex == 1
+          ? FloatingActionButton(
+              backgroundColor: AppColors.secondary,
+              tooltip: 'Add Custom Book',
+              onPressed: () {
+                // TODO: Navigate to AddBookScreen
+              },
+              child: const Icon(Icons.add),
+            )
+          : null,
+    );
+  }
+}
